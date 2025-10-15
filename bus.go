ee4be48f7d2dc67c2eb257f7c8f90137bc81097a
Ralf Wirdemann <ralf.wirdemann@mailbox.org>
2025-10-15 21:03:02 +0200

    Handle FC4 requests

--- bus.go
+++ bus.go
@@ -7,23 +7,39 @@ import (
 	"github.com/rwirdemann/modbuslabs/pkg/modbus"
 )
 
+// Bus represents a Modbus bus.
 type Bus struct {
 	handler      TransportHandler
 	protocolPort ProtocolPort
 }
 
+// NewBus creates a new Modbus bus.
 func NewBus(handler TransportHandler, protocolPort ProtocolPort) *Bus {
 	return &Bus{handler: handler, protocolPort: protocolPort}
 }
 
+// Start starts the Modbus bus.
 func (m *Bus) Start(ctx context.Context) error {
 	return m.handler.Start(ctx, m.processPDU)
 }
 
+// Stop stops the Modbus bus.
 func (m *Bus) Stop() error {
 	return m.handler.Stop()
 }
 
 func (h *Bus) processPDU(registerAddress uint16, pdu modbus.PDU) {
 	slog.Debug("processPDU", "regAddr", registerAddress, "pdu", pdu)
+}
+
+func (h *Bus) AddSlave(unitID uint8) {
+
+}
+
+func (h *Bus) ConnectSlave(unitID uint8) {
+
+}
+
+func (h *Bus) DisconnectSlave(unitID uint8) {
+
 }
--- tcp/handler.go
+++ tcp/handler.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"io"
 	"log/slog"
+	"math"
+	"math/rand"
 	"net"
 	"strings"
 
@@ -74,10 +76,15 @@ func (h *Handler) startRequestCycle(ctx context.Context, processPDU modbuslabs.P
 		case <-ctx.Done():
 			return
 		default:
+			slog.Debug("listening...")
 			conn, err := h.listener.Accept()
 			if err == nil {
 				slog.Debug("client connected", "remote addr", conn.RemoteAddr())
-				h.processRequest(conn, processPDU)
+				for {
+					if err := h.processRequest(conn, processPDU); err != nil {
+						break
+					}
+				}
 			}
 		}
 	}
@@ -95,13 +102,92 @@ func (h *Handler) processRequest(conn net.Conn, processPDU modbuslabs.ProcessPDU
 	slog.Debug("MBAP header received", "pdu", pdu, "txid", txnId)
 	h.protocolPort.Info(fmt.Sprintf("TX % X % X % X", header, pdu.FunctionCode, pdu.Payload))
 
-	processPDU(3, *pdu)
+	addr := modbus.BytesToUint16(pdu.Payload[0:2])
+	processPDU(addr, *pdu)
 
-	payload := modbus.AssembleMBAPFrame(txnId, pdu)
-	if _, err := conn.Write(payload); err != nil {
-		return err
+	var res *modbus.PDU
+	if pdu.FunctionCode == modbus.FC2ReadDiscreteInput {
+		res = &modbus.PDU{
+			UnitId:       pdu.UnitId,
+			FunctionCode: pdu.FunctionCode,
+			Payload:      []byte{0},
+		}
+		quantity := modbus.BytesToUint16(pdu.Payload[2:4])
+		var values = make([]bool, quantity)
+		for i := range int(quantity) {
+			values[i] = rand.Intn(2) == 1
+		}
+		resCount := len(values)
+		// byte count (1 byte for 8 coils)
+		res.Payload[0] = uint8(resCount / 8)
+		if resCount%8 != 0 {
+			res.Payload[0]++
+		}
+		// coil values
+		res.Payload = append(res.Payload, modbus.EncodeBools(values)...)
 	}
-	slog.Debug(fmt.Sprintf("MBAP response written: % X", payload))
-	h.protocolPort.Info(fmt.Sprintf("RX % X", payload))
+
+	if pdu.FunctionCode == modbus.FC4ReadInputRegisters {
+		quantity := modbus.BytesToUint16(pdu.Payload[2:4])
+		slog.Info("FC4 Request", "txnId", txnId, "addr", addr, "quantity", quantity)
+
+		res = &modbus.PDU{
+			UnitId:       pdu.UnitId,
+			FunctionCode: pdu.FunctionCode,
+			Payload:      []byte{uint8(quantity * 2)}, // byte count (2 bytes per register)
+		}
+
+		// Generate float32 values (2 registers per float)
+		// Each float needs 2 consecutive registers (4 bytes)
+		numFloats := int(quantity) / 2
+		for i := 0; i < numFloats; i++ {
+			// Generate a random float between 0 and 100
+			floatValue := rand.Float32() * 100.0
+
+			// Convert float32 to bytes (big endian)
+			bits := math.Float32bits(floatValue)
+			res.Payload = append(res.Payload,
+				byte(bits>>24),
+				byte(bits>>16),
+				byte(bits>>8),
+				byte(bits))
+		}
+
+		// Handle odd number of registers (if quantity is odd)
+		if int(quantity)%2 != 0 {
+			// Add one more register with a random uint16 value
+			value := uint16(rand.Intn(65536))
+			res.Payload = append(res.Payload, modbus.Uint16ToBytes(value)...)
+		}
+
+		// Timesync hack
+		timeregAddr := []byte{0x8F, 0xFC}
+		if addr == modbus.BytesToUint16(timeregAddr) {
+			var syncTime uint64 = 2815470101985099801 // 2025-08-14 15:36
+
+			// Split into 4 words (16-bit each, big endian)
+			word0 := uint16((syncTime >> 48) & 0xFFFF)
+			word1 := uint16((syncTime >> 32) & 0xFFFF)
+			word2 := uint16((syncTime >> 16) & 0xFFFF)
+			word3 := uint16(syncTime & 0xFFFF)
+
+			// Copy the 4 words into the first 8 bytes of res.payload
+			copy(res.Payload[1:3], modbus.Uint16ToBytes(word0))
+			copy(res.Payload[3:5], modbus.Uint16ToBytes(word1))
+			copy(res.Payload[5:7], modbus.Uint16ToBytes(word2))
+			copy(res.Payload[7:9], modbus.Uint16ToBytes(word3))
+		}
+	}
+
+	slog.Info("Handling", "fc", pdu.FunctionCode)
+
+	if res != nil {
+		payload := modbus.AssembleMBAPFrame(txnId, res)
+		if _, err := conn.Write(payload); err != nil {
+			return err
+		}
+		slog.Debug(fmt.Sprintf("MBAP response written: % X", payload))
+		h.protocolPort.Info(fmt.Sprintf("RX % X", payload))
+	}
 	return nil
 }
